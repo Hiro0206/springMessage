c4a91330951a318ea04c541b4b33fa469ea639f5
tao hirokazu <musiclovers.hirohiro@gmail.com>
2020-06-20 11:49:57 +0900

    不要なvalidationアノテーションを削除

--- src/main/java/com/example/LoginForm.java
+++ src/main/java/com/example/LoginForm.java
@@ -1,7 +1,6 @@
 package com.example;
 
 import java.io.Serializable;
-import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -12,9 +11,9 @@ public class LoginForm implements Serializable {
 
   private static final long serialVersionUID = 1L;
 
+  @Size(max = 10)
   private String id;
 
-  @NotEmpty
-  @Size(min = 1, max = 20)
+  @Size(max = 20)
   private String pass;
 }
